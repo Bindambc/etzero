c246b82ff7e8edfa91c7bdab001603807e307c3b
bindambc <bindambc@hotmail.com>
2016-05-01 04:02:11 -0300

    Updates e adicionado instalador

--- src/br/com/etzero/dao/crud/connection/DBConnection.java
+++ src/br/com/etzero/dao/crud/connection/DBConnection.java
@@ -1,28 +1,28 @@
 package br.com.etzero.dao.crud.connection;
 
-import java.io.File;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import javax.swing.JOptionPane;
 
 public class DBConnection {
 
 	public Connection geraConexao() {
 		Connection conexao = null;
-		String dir = null;
 		
-		//use essa para o arquivo jar
+		/*
+		 * Para testar o aplicativo dentro do eclipse utilize o código abaixo:
+		 */
+		// String url =
+		// "jdbc:sqlite:"+System.getProperty("user.dir")+"/database/etzerodata.sdb";
 		
-		
-	//	JOptionPane.showMessageDialog(null, "jdbc:sqlite:"+System.getProperty("user.dir")+"/database/etzerodata.sdb");;
-		String url = "jdbc:sqlite:"+System.getProperty("user.dir")+"/database/etzerodata.sdb";
-	
-		//use essa para executar dentro do eclipse
-		//String url = "jdbc:sqlite:./database/etzerodata.sdb";
 
+		/*
+		 * Para trabalhar com linux utilize o código abaixo:
+		 * 
+		 */
+		String url = "jdbc:sqlite:" + this.getClass().getResource("../../../../../../") + "/database/etzerodata.sdb";
+		
 		try {
 			conexao = DriverManager.getConnection(url);
 		} catch (SQLException e) {
@@ -33,5 +33,4 @@ public class DBConnection {
 
 	}
 
-	
 }
